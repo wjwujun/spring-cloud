727e851e8d2d46cab2e64056ca3e5823d3700ec0
wujun <1477563131@qq.com>
2018-10-26 22:00:09 +0800

    ribbon熔断器

--- spring-cloud-web-ribbon/src/main/java/cn/itcast/springcloudwebribbon/SpringCloudWebRibbonApplication.java
+++ spring-cloud-web-ribbon/src/main/java/cn/itcast/springcloudwebribbon/SpringCloudWebRibbonApplication.java
@@ -3,9 +3,11 @@ package cn.itcast.springcloudwebribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class SpringCloudWebRibbonApplication {
 
     public static void main(String[] args) {
--- spring-cloud-web-ribbon/src/main/java/cn/itcast/springcloudwebribbon/service/AdminService.java
+++ spring-cloud-web-ribbon/src/main/java/cn/itcast/springcloudwebribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package cn.itcast.springcloudwebribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,13 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String sayHi(String message){
         return restTemplate.getForObject("http://spring-cloud-service/hi?message="+message,String.class);
+    }
+
+    public String hiError(String message){
+        return String.format("hi your message is :%s , but request bad!",message);
+
     }
 }
