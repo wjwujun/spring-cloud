36dbf2ea7a009dd765b8314b8ebcdbde1c1aaf68
wujun <1477563131@qq.com>
2018-10-26 22:10:43 +0800

    feign熔断

--- spring-cloud-web-feign/src/main/java/cn/itcast/springcloudwebfeign/service/FeignService.java
+++ spring-cloud-web-feign/src/main/java/cn/itcast/springcloudwebfeign/service/FeignService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "spring-cloud-service")
+@FeignClient(value = "spring-cloud-service",fallback = FeignServiceHystrix.class)
 public interface FeignService {
 
     @GetMapping(value = "hi")
