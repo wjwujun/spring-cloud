2dfb955a2bc0c71565033cb2462112041f8f3d57
wujun <1477563131@qq.com>
2018-10-27 20:58:48 +0800

    Admin服务监控

---
+++ spring-cloud-admin/src/main/java/cn/itcast/springcloudadmin/SpringCloudAdminApplication.java
@@ -0,0 +1,16 @@
+package cn.itcast.springcloudadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class SpringCloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAdminApplication.class, args);
+    }
+}
